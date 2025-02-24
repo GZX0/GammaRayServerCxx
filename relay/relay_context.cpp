ee0c19694033828e2730046c90058021ce05bda9
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2025-02-24 22:24:31 +0800

    update
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

---
+++ relay/relay_context.cpp
@@ -0,0 +1,5 @@
+//
+// Created by RGAA on 24/02/2025.
+//
+
+#include "relay_context.h"
---
+++ relay/relay_context.h
@@ -0,0 +1,17 @@
+//
+// Created by RGAA on 24/02/2025.
+//
+
+#ifndef GAMMARAYSERVER_RELAY_CONTEXT_H
+#define GAMMARAYSERVER_RELAY_CONTEXT_H
+
+namespace tc
+{
+
+    class RelayContext {
+
+    };
+
+}
+
+#endif //GAMMARAYSERVER_RELAY_CONTEXT_H
